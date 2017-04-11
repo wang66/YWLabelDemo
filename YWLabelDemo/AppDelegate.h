9cc0c9ebcdb4f37ff27e756910da73fb642c954e
wang66 <18693133051@163.com>
2017-04-11 16:01:21 +0800

    Initial Commit

---
+++ YWLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWLabelDemo
+//
+//  Created by 王亚文 on 2017/4/11.
+//  Copyright © 2017年 wyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWLabelDemo
+//
+//  Created by 王亚文 on 2017/4/11.
+//  Copyright © 2017年 wyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
